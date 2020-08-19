2e555b81ce21c13b9608a0a9c45f9ddd006ce5bc
Shuai Ruan <rsboss01@outlook.com>
2020-08-19 16:22:46 +0800

    fix tiny bugs

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
+#!/usr/bin/python3
 from main import ftb
 ftb(filename="payment.py", line="128", errorin="pay_process", error="PaymentFailed", errortext="Bad Internet connection occurs payment wasn't done.")
