5bef4ee2eadddaaa2b0b73dca1ca2265c54474c2
Shuai Ruan <rsboss01@outlook.com>
2020-08-19 16:20:45 +0800

    Release 1.0

---
+++ demo.py
@@ -0,0 +1,2 @@
+from main import ftb
+ftb(filename="payment.py", line="128", errorin="pay_process", error="PaymentFailed", errortext="Bad Internet connection occurs payment wasn't done.")
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def ftb(filename, line, errorin, error, errortext):
+    print("Traceback (most recent call last):");
+    print(' File "' + filename + '", line ' + line + " in " + errorin)
+    print(error + ": " + errortext)
+
