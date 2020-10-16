33f0911a99252fec06afcc4540408b54d4d25750
shuairuan <rsow001@gmail.com>
2020-10-16 20:11:38 +0800

    Modified Demo and FTB

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def ftb(filename, line, errorin, error, errortext):
+def ftb(filename="<stdin>", line="1", errorin="handler", error="UnknownError", errortext="Unknown error during handler is working."):
     print("Traceback (most recent call last):");
-    print(' File "' + filename + '", line ' + line + ", in " + errorin)
-    print(error + ": " + errortext)
-
+    print('  File "' + str(filename) + '", line ' + str(line) + ", in " + str(errorin))
+    print(str(error) + ": " + str(errortext))
